98b960be5023a042805cbe7b6f09bbc521d3aa66
Andy Tewfik <Andy.tewfik@gmail.com>
2025-02-25 22:52:34 -0800

    fixing stuff

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { plugin as markdown } from 'vite-plugin-markdown';
+import fs from 'fs'; 
 
 export default defineConfig({
   plugins: [
